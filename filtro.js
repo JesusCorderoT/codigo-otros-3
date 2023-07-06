a4c36f5893163d948337b389de568d403ba9283c
JesusCorderoT <jesus.of.999@gmail.com>
2023-07-06 12:01:30 -0600

    Se arregla el proyecto

--- filtro.js
+++ filtro.js
@@ -1,4 +1,7 @@
 // Tenemos un li de productos
+//Realizo Jesus Cordero Torres, califica Ana Fernanda con 5/5
+
+//Realizo Ana Fernanda, califica Jesus Cordero Torres con 5/5
 
 const productos = [
   {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
@@ -8,24 +11,27 @@ const productos = [
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+const li = document.getElementById("lista-de-productos"); //cambio por getElementById
+const entrada = document.getElementById('entrada'); //cambio por getElementById y se cambió $i por entrada
 
-for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
+const displayProductos = (productos) => { //se metió un for en una función flecha llamada displayProductos
+  for (let i = 0; i < productos.length; i++) {
+    var d = document.createElement("div")
+    d.classList.add("producto")
 
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
-  
-  var imagen = document.createElement("img");
-  imagen.setAttribute('src', productos[i].img);
+    var ti = document.createElement("p")
+    ti.classList.add("titulo")
+    ti.textContent = productos[i].nombre
 
-  d.appendChild(ti)
-  d.appendChild(imagen)
+    var imagen = document.createElement("img");
+    imagen.setAttribute('src', productos[i].img);
 
-  li.appendChild(d)
+    d.appendChild(ti)
+    d.appendChild(imagen)
+
+    li.appendChild(d)
+  }
+
 }
 
 displayProductos(productos)
@@ -36,28 +42,28 @@ botonDeFiltro.onclick = function() {
     li.removeChild(li.firstChild);
   }
 
-  const texto = $i.value;
+  const texto = entrada.value; //se cambio $i por entrada
   console.log(texto);
   const productosFiltrados = filtrado(productos, texto );
 
   for (let i = 0; i < productosFiltrados.length; i++) {
     var d = document.createElement("div")
     d.classList.add("producto")
-  
-    var ti = document.createElement("p")
+
+var ti = document.createElement("p")
     ti.classList.add("titulo")
     ti.textContent = productosFiltrados[i].nombre
-    
+
     var imagen = document.createElement("img");
     imagen.setAttribute('src', productosFiltrados[i].img);
-  
+
     d.appendChild(ti)
     d.appendChild(imagen)
-  
+
     li.appendChild(d)
   }
 }
 
 const filtrado = (productos = [], texto) => {
   return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
-}  
+}
